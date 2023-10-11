c538738b40a9c67580f70c7c1cb3336500baaa84
NaeemaBsharat <naeemabsharat1@gmail.com>
2023-10-11 19:37:10 +0300

    Login-project

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+
+
+document.getElementById('login-button').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the form submission
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            username,
+            password,
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.token) {
+            console.log(data.token); // Print the token to the console
+            // Store the token in a cookie
+            document.cookie = `token=${data.token}`;
+            window.location.href = 'profile.html';
+        } else {
+            console.error('Token not found in the response');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
